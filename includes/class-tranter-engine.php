7e46cbb3ab759fdf7f5b0850e5c0817ef6909936
oddly-fred <fredesumei207@gmail.com>
2026-07-03 15:22:50 +0100

    Register Digital Marketing Brand page module

--- includes/class-tranter-engine.php
+++ includes/class-tranter-engine.php
@@ -22,6 +22,7 @@ class Tranter_Engine {
         require_once TRANTER_ENGINE_PATH . 'includes/it-support-page.php';
         require_once TRANTER_ENGINE_PATH . 'includes/smart-solutions-page.php';
         require_once TRANTER_ENGINE_PATH . 'includes/hr-support-page.php';
+        require_once TRANTER_ENGINE_PATH . 'includes/digital-marketing-brand-page.php';
 
         Tranter_Post_Types::init();
         Tranter_Admin_App::init();
@@ -39,6 +40,7 @@ class Tranter_Engine {
         Tranter_IT_Support_Page::init();
         Tranter_Smart_Solutions_Page::init();
         Tranter_HR_Support_Page::init();
+        Tranter_Digital_Marketing_Brand_Page::init();
         Tranter_Seeder::ensure_sections();
         if (method_exists('Tranter_Seeder', 'migrate_106')) Tranter_Seeder::migrate_106();
         if (method_exists('Tranter_Seeder', 'migrate_109')) Tranter_Seeder::migrate_109();
@@ -57,6 +59,7 @@ class Tranter_Engine {
         require_once TRANTER_ENGINE_PATH . 'includes/it-support-page.php';
         require_once TRANTER_ENGINE_PATH . 'includes/smart-solutions-page.php';
         require_once TRANTER_ENGINE_PATH . 'includes/hr-support-page.php';
+        require_once TRANTER_ENGINE_PATH . 'includes/digital-marketing-brand-page.php';
         Tranter_Post_Types::register();
         Tranter_Seeder::seed();
         if (class_exists('Tranter_Knowledge_Hub')) Tranter_Knowledge_Hub::activation_setup();
