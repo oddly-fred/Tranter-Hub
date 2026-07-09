1a1c40a06e4e4e9be073e2f97bb12cda1e105e8a
oddly-fred <fredesumei207@gmail.com>
2026-07-09 12:13:05 +0100

    Add Header v3 routing correction map

---
+++ plugins/tranter-hub-header-v3/fixes/routing-correction-map.js
@@ -0,0 +1,23 @@
+/*
+ * Header v3.0 routing correction map.
+ * This documents the approved Tranter Hub header routing updates included in the preview ZIP.
+ */
+(function(){
+  window.TRANTER_HEADER_ROUTING_MAP = {
+    whoWeAre: '/who-we-are/',
+    whatWeDo: '/what-we-do/',
+    viewAllServices: '/what-we-do/',
+    zohoSolutions: '/zoho-solutions/',
+    manageEngine: '/manage-engine/',
+    sophos: '/sophos/',
+    partners: '/partners/',
+    insights: '/insights/',
+    contact: '/contact/',
+    zohoSolutionGroup: {
+      crm: '/zoho-crm/',
+      books: '/zoho-books/',
+      desk: '/zoho-desk/',
+      workplace: '/zoho-workplace/'
+    }
+  };
+})();
